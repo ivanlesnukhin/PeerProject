3a45a28b2baf965c74f3dcbbe66bc2c88703c327
Max <maxarfvidsson@gmail.com>
2019-12-11 14:10:41 +0100

    removing and rearanching basicRobotFeatureSystem

--- features/Assignment5/src/BasicRobotFeatureSystem.java
+++ features/Assignment5/src/BasicRobotFeatureSystem.java
@@ -1,41 +1,14 @@
 import robocode.*;
 
 import java.awt.*;
-import java.awt.geom.Point2D;
-import java.util.ArrayList;
 import java.io.FileReader;
-import java.util.Properties; 
-
-/**
- * A combination of the movement from BasicSurfer and the gun from 
- * GFTargetingBot. See:
- *   http://robowiki.net?BasicSurfer
- *   http://robowiki.net?GFTargetingBot
- *
- * Change log:
- *   1.01 - solved bug in distance-bin calculation when distance > 900
- *   1.02 - fixed wave mismatching caused by rounding errors
- */
+import java.util.Properties;
 
 public class BasicRobotFeatureSystem extends AdvancedRobot {
 	IMovement movementMeth;
-	//#if WaveSurfing
-    public static int BINS = 47;
-    public static double _surfStats[] = new double[BINS]; // we'll use 47 bins
-    public Point2D.Double _myLocation;     // our bot's location
-    public Point2D.Double _enemyLocation;  // enemy bot's location
 
-    public ArrayList _enemyWaves;
-    public ArrayList _surfDirections;
-    public ArrayList _surfAbsBearings;
-    //#endif
 	public IFiring firingMethod;
-	//#if Random
-//@	private static final double BULLET_POWER = 1.9;
-    //#else
     private static double BULLET_POWER = 1.0;
-	//#endif
-    
 	private static double lateralDirection;
 	private static double lastEnemyVelocity;
 	
@@ -49,28 +22,15 @@ public class BasicRobotFeatureSystem extends AdvancedRobot {
 			RBY = false,
 			GWY = false;
     
-    //#if Random
-//@    private static GFTMovement movement;
-//@    
-//@	public BasicRobotFeatureSystem() {
-//@		movement = new GFTMovement(this);	
-//@	}
-	//#endif
-	
-	
-	boolean stringToBoolean (String input) {
-		if(input == "false") 
-			return false;
-		else 
-			return true;
-	}
 
-    public void run() {
-    	try (FileReader reader = new FileReader("runtime.properties")) {
-    		Properties properties = new Properties();
-    		properties.load(reader);
-    		
-    		Wall_Smoothing = stringToBoolean(properties.getProperty("Wall_Smoothing"));
+	public BasicRobotFeatureSystem() {
+
+		//////--------    Reads properties from file and converts into booleans
+		try (FileReader reader = new FileReader("runtime.properties")) {
+			Properties properties = new Properties();
+			properties.load(reader);
+
+			Wall_Smoothing = stringToBoolean(properties.getProperty("Wall_Smoothing"));
 			visibleEnemyWaves = stringToBoolean(properties.getProperty("visibleEnemyWaves"));
 			Random = stringToBoolean(properties.getProperty("Random"));
 			GF = stringToBoolean(properties.getProperty("GF"));
@@ -78,23 +38,27 @@ public class BasicRobotFeatureSystem extends AdvancedRobot {
 			BBY = stringToBoolean(properties.getProperty("BBY "));
 			RBY = stringToBoolean(properties.getProperty("RBY"));
 			GWY = stringToBoolean(properties.getProperty("GWY"));
-    	}catch (Exception e){
-    		e.printStackTrace();
-    	}
-    	
-    	if (BBY)
-    		setColors(Color.BLUE, Color.BLACK, Color.YELLOW);
-    	else if (RBY)
-    		setColors(Color.RED, Color.BLACK, Color.WHITE);
-    	else if (GWY)
-    		setColors(Color.GREEN, Color.WHITE, Color.YELLOW);
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		/////-------------
 
-    	if(WaveSurfing) {
-    		movementMeth = new WaveSurfing(this);
-    	} else if(Random) {
-    		movementMeth = new RandomMovement(this);
-    	} 
 
+		/////------------- Sets color
+		if (BBY)
+			setColors(Color.BLUE, Color.BLACK, Color.YELLOW);
+		else if (RBY)
+			setColors(Color.RED, Color.BLACK, Color.WHITE);
+		else if (GWY)
+			setColors(Color.GREEN, Color.WHITE, Color.YELLOW);
+		/////-------------
+
+
+		if(WaveSurfing) {
+			movementMeth = new WaveSurfing(this);
+		} else if(Random) {
+			movementMeth = new RandomMovement(this);
+		}
 
 		lateralDirection = 1;
 		lastEnemyVelocity = 0;
@@ -108,25 +72,22 @@ public class BasicRobotFeatureSystem extends AdvancedRobot {
 			}
 			firingMethod = new GuessFactor(this, lastEnemyVelocity, lateralDirection, movementMethod);
 		}
-    }
 
-	    //#if DBL_Bullet_Power
-//@		BULLET_POWER = 1.9;
-		//#endif
-		
-		
-		//#if Random
-//@		setAdjustRadarForGunTurn(true);
-//@		setAdjustGunForRobotTurn(true);
-//@		do {
-//@			turnRadarRightRadians(Double.POSITIVE_INFINITY); 
-//@		} while (true);
-//@	}
-//@ 
-//@	public void onScannedRobot(ScannedRobotEvent e) {
-		//#endif
-		
-		//#if WaveSurfing
+		if (DBL_Bullet_Power)
+			BULLET_POWER = 1.9;
+	}
+	
+	
+	boolean stringToBoolean (String input) {
+		if(input == "false") 
+			return false;
+		else 
+			return true;
+	}
+
+    public void run() {
+
+    }
 
     public void onScannedRobot(ScannedRobotEvent e) {
     	movementMeth.onScannedRobot(e);
--- features/Assignment5/src/RandomMovement.java
+++ features/Assignment5/src/RandomMovement.java
@@ -24,11 +24,20 @@ class RandomMovement implements IMovement{
 	private double direction = 0.4;
  
 	RandomMovement(AdvancedRobot _robot) {
+
 		this.robot = _robot;
+
+		robot.setAdjustRadarForGunTurn(true);
+		robot.setAdjustGunForRobotTurn(true);
+		do {
+			robot.turnRadarRightRadians(Double.POSITIVE_INFINITY);
+		} while (true);
 	}
  
 	@Override
 	public void onScannedRobot(ScannedRobotEvent e) {
+
+
 		double enemyAbsoluteBearing = robot.getHeadingRadians() + e.getBearingRadians();
 		double enemyDistance = e.getDistance();
 		Point2D robotLocation = new Point2D.Double(robot.getX(), robot.getY());
@@ -47,5 +56,28 @@ class RandomMovement implements IMovement{
 	double angle = GFTUtils.absoluteBearing(robotLocation, robotDestination) - robot.getHeadingRadians();
 	robot.setAhead(Math.cos(angle) * 100);
 	robot.setTurnRightRadians(Math.tan(angle));
+	}
+}
+
+class GFTUtils {
+	static double bulletVelocity(double power) {
+		return 20 - 3 * power;
+	}
+
+	static Point2D project(Point2D sourceLocation, double angle, double length) {
+		return new Point2D.Double(sourceLocation.getX() + Math.sin(angle) * length,
+				sourceLocation.getY() + Math.cos(angle) * length);
+	}
+
+	static double absoluteBearing(Point2D source, Point2D target) {
+		return Math.atan2(target.getX() - source.getX(), target.getY() - source.getY());
+	}
+
+	static int sign(double v) {
+		return v < 0 ? -1 : 1;
+	}
+
+	static int minMax(int v, int min, int max) {
+		return Math.max(min, Math.min(max, v));
 	}
 }
