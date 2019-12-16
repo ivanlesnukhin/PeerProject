d586f084e48a6c2913695f80de8fb30638e3df8b
ivanlesnukhin <ivan.lesnukhin@gmail.com>
2019-12-16 13:01:25 +0100

    LT testing

--- features/Assignment5/src/MyTestClass.java
+++ features/Assignment5/src/MyTestClass.java
@@ -4,32 +4,41 @@ import org.junit.jupiter.api.Test;
 
 
 class MyTestClass {
-	private AdvancedRobot robot = new AdvancedRobot();
-	private IFiring firing = new LinearTargeting(robot);
 	
- 
+	//Perform test for Linear Targeting (LT) feature
+	private AdvancedRobot robotLT = new AdvancedRobot();
+	private IFiring firing = new LinearTargeting(robotLT);
 	
+	/**
+	 * Tests whether or not the robot changes its gun angle
+	 */
 	@Test
 	void testSetTurnGunRightRadians() {
-		double gunAngle = 2.4;
-		double beforeAngle = robot.getHeadingRadians();
+		double oldAngle = robotLT.getHeadingRadians();
 		
-		firing.setTurnGunRightRadians(gunAngle);
-		double currentAngle = robot.getHeadingRadians();
+		double deltaGunAngle = 2.4;
+		firing.setTurnGunRightRadians(deltaGunAngle);
+		double newAngle = robotLT.getHeadingRadians();
 		
-		assertNotSame(beforeAngle, currentAngle);
-		assertSame(currentAngle, beforeAngle+gunAngle);
+		assertNotSame(oldAngle, newAngle);
+		assertSame(newAngle, oldAngle + deltaGunAngle);
 	}
 	
+	/**
+	 * Tests whether or not the robot changes its radar angle
+	 */
 	@Test
 	void testSetTurnRadarRightRadians() {
-		double radarAngle = 2.4;
-		double beforeAngle = robot.getRadarHeadingRadians();
 		
-		firing.setTurnGunRightRadians(radarAngle);
-		double currentAngle = robot.getRadarHeadingRadians();
+		double oldAngle = robotLT.getRadarHeadingRadians();
 		
-		assertNotSame(beforeAngle, currentAngle);
-		assertSame(currentAngle, beforeAngle+radarAngle);
+		double deltaRadarAngle = 2.4;
+		firing.setTurnRadarRightRadians(deltaRadarAngle);
+		double newAngle = robotLT.getRadarHeadingRadians();
+		
+		assertNotSame(oldAngle, newAngle);
+		assertSame(newAngle, oldAngle + deltaRadarAngle);
 	}
+	
+	
 }
