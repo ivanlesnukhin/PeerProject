37d509b669d53c9bb4355d771f636af7aa86a299
ivanlesnukhin <ivan.lesnukhin@gmail.com>
2019-12-16 12:41:05 +0100

    modified test-class

--- features/Assignment5/src/MyTestClass.java
+++ features/Assignment5/src/MyTestClass.java
@@ -1,12 +1,41 @@
+import robocode.*;
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
+
 class MyTestClass {
-
+	private AdvancedRobot robot = new AdvancedRobot();
+	private IFiring firing = new LinearTargeting(robot);
+	
+ 
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testSetFire() {
+		double firePower = 5.1;
+		firing.setFire(firePower);
 	}
-
+	
+	@Test
+	void testSetTurnGunRightRadians() {
+		double gunAngle = 2.4;
+		double beforeAngle = robot.getHeadingRadians();
+		
+		firing.setTurnGunRightRadians(gunAngle);
+		double currentAngle = robot.getHeadingRadians();
+		
+		assertNotSame(beforeAngle, currentAngle);
+		assertSame(currentAngle, gunAngle);
+	}
+	
+	@Test
+	void testSetTurnRadarRightRadians() {
+		double radarAngle = 2.4;
+		double beforeAngle = robot.getRadarHeadingRadians();
+		
+		firing.setTurnGunRightRadians(radarAngle);
+		double currentAngle = robot.getRadarHeadingRadians();
+		
+		assertNotSame(beforeAngle, currentAngle);
+		assertSame(currentAngle, radarAngle);
+	}
 }
